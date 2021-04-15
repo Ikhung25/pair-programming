5e3adbf125dfdd31fb015c73e8b509f857002836
ikhung25 <ikhung25@gmail.com>
2021-04-15 15:12:41 -0400

    interface

--- untitled/src/Rectangle.java
+++ untitled/src/Rectangle.java
@@ -1,2 +1,14 @@
-public class Rectangle {
+public class Rectangle implements IShape{
+    double width;
+    double length;
+
+    @Override
+    public double getPerimeter() {
+        return 0;
+    }
+
+    @Override
+    public double getArea() {
+        return 0;
+    }
 }
