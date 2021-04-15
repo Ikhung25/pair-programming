fcf8c48e02638047fbe8c896cde98a6ac5e114db
Drew <amarnold@wpi.edu>
2021-04-15 15:35:55 -0400

    Rectangle constructor

--- untitled/src/Rectangle.java
+++ untitled/src/Rectangle.java
@@ -2,6 +2,11 @@ public class Rectangle implements IShape{
     double width;
     double length;
 
+    public Rectangle(double width, double length) {
+        this.width = width;
+        this.length = length;
+    }
+
     @Override
     public double getPerimeter() {
         return (2 * width) + (2 * length);
